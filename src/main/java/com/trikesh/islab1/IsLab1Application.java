0ecb85a64b8d169c1ece64d2958ad540b85cb746
trikesh <trikesh12344321@gmail.com>
2025-09-25 20:45:35 +0300

    Initial commit

---
+++ src/main/java/com/trikesh/islab1/IsLab1Application.java
@@ -0,0 +1,13 @@
+package com.trikesh.islab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsLab1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsLab1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/trikesh/islab1/IsLab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trikesh.islab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsLab1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
